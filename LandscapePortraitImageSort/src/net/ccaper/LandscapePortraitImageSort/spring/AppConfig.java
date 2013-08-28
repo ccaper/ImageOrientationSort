19cc37ff26ded872baff5ee7bd62173fc4d2f594
ccaper <christian.caperton@gmail.com>
2013-08-28 22:52:25 +0000

    Wiring up Spring Java Config

---
+++ LandscapePortraitImageSort/src/net/ccaper/LandscapePortraitImageSort/spring/AppConfig.java
@@ -0,0 +1,10 @@
+package net.ccaper.LandscapePortraitImageSort.spring;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.ImportResource;
+
+@Configuration
+@ImportResource("classpath:/net/ccaper/LandscapePortraitImageSort.spring.properties-config.xml")
+public class AppConfig {
+
+}
