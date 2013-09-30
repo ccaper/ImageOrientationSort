b7187b38354494844ec145ab4a6d8a00ff602216
ccaper <christian.caperton@gmail.com>
2013-09-30 23:03:49 +0000

    adding class that decides image orientation

---
+++ LandscapePortraitImageSort/src/net/ccaper/LandscapePortraitImageSort/util/LandscapePortriatUtils.java
@@ -0,0 +1,62 @@
+package net.ccaper.LandscapePortraitImageSort.util;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Iterator;
+
+import javax.imageio.ImageIO;
+import javax.imageio.ImageReader;
+import javax.imageio.stream.FileImageInputStream;
+import javax.imageio.stream.ImageInputStream;
+
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+
+public class LandscapePortriatUtils {
+  private static final Log LOG = LogFactory
+      .getLog(LandscapePortriatUtils.class);
+
+  public enum Orientation {
+    LANDSCAPE, PORTRAIT, EQUAL
+  };
+
+  // TODO: junit
+  // TODO: refactor
+  public static Orientation getOrientation(File file) {
+    int height = 0;
+    int width = 0;
+    String suffix = file.getName().substring(
+        file.getName().lastIndexOf('.') + 1);
+    Iterator<ImageReader> iter = ImageIO.getImageReadersBySuffix(suffix);
+    if (iter.hasNext()) {
+      ImageReader reader = iter.next();
+      ImageInputStream stream = null;
+      try {
+        stream = new FileImageInputStream(file);
+        reader.setInput(stream);
+        width = reader.getWidth(reader.getMinIndex());
+        height = reader.getHeight(reader.getMinIndex());
+      } catch (IOException e) {
+        LOG.error(
+            String.format("IOException while reading %s.",
+                file.getAbsoluteFile()), e);
+      } finally {
+        reader.dispose();
+        try {
+          stream.close();
+        } catch (IOException e) {
+          LOG.error(
+              String.format("IOException while closing %s.",
+                  file.getAbsoluteFile()), e);
+        }
+      }
+    }
+    if (height > width) {
+      return Orientation.PORTRAIT;
+    } else if (height < width) {
+      return Orientation.LANDSCAPE;
+    } else {
+      return Orientation.EQUAL;
+    }
+  }
+}
