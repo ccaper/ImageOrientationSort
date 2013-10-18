01e30a7b1f53f5387619511ba400a71229d28206
ccaper <christian.caperton@gmail.com>
2013-10-18 23:20:22 +0000

    adding more test coverage around Driver

--- ImageOrientationSort/test/net/ccaper/ImageOrientationSort/DriverTest.java
+++ ImageOrientationSort/test/net/ccaper/ImageOrientationSort/DriverTest.java
@@ -67,4 +67,92 @@ public class DriverTest {
     Driver.isWritable(fileMock);
     verify(fileMock);
   }
+
+  @Test
+  public void validateStartAndDestinationDirectories_HappyPath()
+      throws Exception {
+    File startDirectoryMock = createMock(File.class);
+    expect(startDirectoryMock.exists()).andReturn(true);
+    expect(startDirectoryMock.canRead()).andReturn(true);
+    replay(startDirectoryMock);
+    File destinationDirectoryMock = createMock(File.class);
+    expect(destinationDirectoryMock.exists()).andReturn(true);
+    expect(destinationDirectoryMock.canWrite()).andReturn(true);
+    replay(destinationDirectoryMock);
+    Driver driver = new Driver(startDirectoryMock, destinationDirectoryMock,
+        null, null, null, null, null, null);
+    driver.validateStartAndDestinationDirectories();
+    verify(startDirectoryMock);
+    verify(destinationDirectoryMock);
+  }
+
+  @Test(expected = Exception.class)
+  public void validateStartAndDestinationDirectories_SadPath_StartDirNotExists()
+      throws Exception {
+    File startDirectoryMock = createMock(File.class);
+    expect(startDirectoryMock.exists()).andReturn(false);
+    expect(startDirectoryMock.getAbsolutePath()).andReturn("/some/dir").times(2);
+    replay(startDirectoryMock);
+    File destinationDirectoryMock = createMock(File.class);
+    replay(destinationDirectoryMock);
+    Driver driver = new Driver(startDirectoryMock, destinationDirectoryMock,
+        null, null, null, null, null, null);
+    driver.validateStartAndDestinationDirectories();
+    verify(startDirectoryMock);
+    verify(destinationDirectoryMock);
+  }
+
+  @Test(expected = Exception.class)
+  public void validateStartAndDestinationDirectories_SadPath_StartDirCannotRead()
+      throws Exception {
+    File startDirectoryMock = createMock(File.class);
+    expect(startDirectoryMock.exists()).andReturn(true);
+    expect(startDirectoryMock.canRead()).andReturn(false);
+    expect(startDirectoryMock.getAbsolutePath()).andReturn("/some/dir").times(2);
+    replay(startDirectoryMock);
+    File destinationDirectoryMock = createMock(File.class);
+    replay(destinationDirectoryMock);
+    Driver driver = new Driver(startDirectoryMock, destinationDirectoryMock,
+        null, null, null, null, null, null);
+    driver.validateStartAndDestinationDirectories();
+    verify(startDirectoryMock);
+    verify(destinationDirectoryMock);
+  }
+
+  @Test(expected = Exception.class)
+  public void validateStartAndDestinationDirectories_SadPath_DestDirNotExists()
+      throws Exception {
+    File startDirectoryMock = createMock(File.class);
+    expect(startDirectoryMock.exists()).andReturn(true);
+    expect(startDirectoryMock.canRead()).andReturn(true);
+    replay(startDirectoryMock);
+    File destinationDirectoryMock = createMock(File.class);
+    expect(destinationDirectoryMock.exists()).andReturn(false);
+    expect(destinationDirectoryMock.getAbsolutePath()).andReturn("/some/dir").times(2);
+    replay(destinationDirectoryMock);
+    Driver driver = new Driver(startDirectoryMock, destinationDirectoryMock,
+        null, null, null, null, null, null);
+    driver.validateStartAndDestinationDirectories();
+    verify(startDirectoryMock);
+    verify(destinationDirectoryMock);
+  }
+
+  @Test(expected = Exception.class)
+  public void validateStartAndDestinationDirectories_SadPath_DestDirCannotWrite()
+      throws Exception {
+    File startDirectoryMock = createMock(File.class);
+    expect(startDirectoryMock.exists()).andReturn(true);
+    expect(startDirectoryMock.canRead()).andReturn(true);
+    replay(startDirectoryMock);
+    File destinationDirectoryMock = createMock(File.class);
+    expect(destinationDirectoryMock.exists()).andReturn(true);
+    expect(destinationDirectoryMock.canWrite()).andReturn(false);
+    expect(destinationDirectoryMock.getAbsolutePath()).andReturn("/some/dir").times(2);
+    replay(destinationDirectoryMock);
+    Driver driver = new Driver(startDirectoryMock, destinationDirectoryMock,
+        null, null, null, null, null, null);
+    driver.validateStartAndDestinationDirectories();
+    verify(startDirectoryMock);
+    verify(destinationDirectoryMock);
+  }
 }
